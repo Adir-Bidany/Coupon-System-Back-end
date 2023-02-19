24ee884b0e1edc565b25852ad5673735df80d1e1
Adir Bidany <Adirharel12345@gmail.com>
2023-02-19 08:48:42 +0200

    Conected to front

--- src/main/java/com/jb/couponSys/controller/CustomerController.java
+++ src/main/java/com/jb/couponSys/controller/CustomerController.java
@@ -16,24 +16,24 @@ import java.util.List;
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
-    
-    @PostMapping("{customerId}/coupons/{couponId}")//
+
+    @PostMapping("{customerId}/coupons/{couponId}")
     @ResponseStatus(HttpStatus.CREATED)
     public void purchaseCoupon(@PathVariable int customerId, @PathVariable int couponId) throws CouponSysException {
         customerService.purchaseCoupon(customerId, couponId);
     }
 
-    @GetMapping("{customerId}/coupons")//
+    @GetMapping("{customerId}/coupons")
     public List<Coupon> getAllCustomerPurchasedCoupons(@PathVariable int customerId) throws CouponSysException {
         return customerService.getAllCustomerPurchasedCoupons(customerId);
     }
 
-    @GetMapping("{customerId}/coupons/category")//
+    @GetMapping("{customerId}/coupons/category")
     public List<Coupon> getAllCustomerPurchasedCouponsByCategory(@PathVariable int customerId, @RequestParam String category) throws CouponSysException {
         return customerService.getAllCustomerPurchasedCouponsByCategory(customerId, Category.valueOf(category));
     }
 
-    @GetMapping("{customerId}/coupons/price/max")//
+    @GetMapping("{customerId}/coupons/price/max")
     public List<Coupon> getAllCustomerPurchasedCouponsByMaxPrice(@PathVariable int customerId, @RequestParam double maxPrice) throws CouponSysException {
         return customerService.getAllCustomerPurchasedCouponsByMaxPrice(customerId, maxPrice);
     }
--- src/main/java/com/jb/couponSys/service/CompanyService.java
+++ src/main/java/com/jb/couponSys/service/CompanyService.java
@@ -9,12 +9,20 @@ import java.util.List;
 
 public interface CompanyService {
     boolean login(String email, String password) throws CouponSysException;
+
     void addCoupon(Coupon coupon) throws CouponSysException;
-    void updateCoupon(int couponId,Coupon coupon) throws CouponSysException;
+
+    void updateCoupon(int couponId, Coupon coupon) throws CouponSysException;
+
     void deleteCoupon(int couponId) throws CouponSysException;
+
     Coupon getSingleCoupon(int couponId) throws CouponSysException;
+
     List<Coupon> getAllCompanyCoupons(int companyId) throws CouponSysException;
+
     List<Coupon> getAllCompanyCouponsByCategory(int companyId, Category category) throws CouponSysException;
+
     List<Coupon> getAllCompanyCouponsByMaxPrice(int companyId, double maxPrice) throws CouponSysException;
+
     Company getLoginCompany(int companyId) throws CouponSysException;
 }
